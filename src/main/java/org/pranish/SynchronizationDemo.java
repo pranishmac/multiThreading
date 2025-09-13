1b7df344d229e27ed3ca12be5f1ff4caa8104734
pranishmac <pranish.mac321@gmail.com>
2025-09-13 13:28:37 +0530

    Synchronized method implementation

---
+++ src/main/java/org/pranish/SynchronizationDemo.java
@@ -0,0 +1,36 @@
+package org.pranish;
+
+public class SynchronizationDemo {
+    private static Integer count = 0;
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(()->{
+            for (int i = 0; i < 1000; i++) {
+                increment();
+            }
+        });
+
+        Thread t2 = new Thread(()->{
+            for (int i = 0; i < 1000; i++) {
+                increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(count);
+
+    }
+
+    private synchronized static void increment(){
+        count++;
+    }
+
+}
